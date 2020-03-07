9b3e5adefe2395a2e1eba79df7bc30b7aaa01ee6
LetitiaJD <letitiajdawson@gmail.com>
2020-03-07 21:26:01 +0100

    Registered resourceful routes for Account, Category and Entry Controllers

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('account', 'AccountController');
+
+Route::resource('category', 'CategoryController');
+
+Route::resource('entry', 'EntryController');
