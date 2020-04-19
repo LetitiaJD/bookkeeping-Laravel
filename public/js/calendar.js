fa3201f2cfaa83fac647027bb382d9f143c7cbfa
LetitiaJD <letitiajdawson@gmail.com>
2020-04-19 17:12:37 +0200

    Added css for table to vertically and horizonally align cells and added a border. Also added a JS-DateRangePicker

---
+++ public/js/calendar.js
@@ -0,0 +1,52 @@
+$('#calendarRange').daterangepicker({
+    "showDropdowns": true,
+    "showWeekNumbers": true,
+    "autoApply": true,
+    ranges: {
+        'Today': [moment(), moment()],
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    },
+    "locale": {
+        "format": "MM/DD/YYYY",
+        "separator": " - ",
+        "applyLabel": "Apply",
+        "cancelLabel": "Cancel",
+        "fromLabel": "From",
+        "toLabel": "To",
+        "customRangeLabel": "Custom",
+        "weekLabel": "W",
+        "daysOfWeek": [
+            "Su",
+            "Mo",
+            "Tu",
+            "We",
+            "Th",
+            "Fr",
+            "Sa"
+        ],
+        "monthNames": [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"
+        ],
+        "firstDay": 1
+    },
+    "alwaysShowCalendars": true,
+    "startDate": "04/13/2020",
+    "endDate": "04/19/2020"
+}, function(start, end, label) {
+  console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+});
--- resources/views/entry/index.blade.php
+++ resources/views/entry/index.blade.php
@@ -5,13 +5,13 @@
 <div class="row">
     <div class="col-12">
         @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
         @elseif(session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
+        <div class="alert alert-danger">
+            {{ session('error') }}
+        </div>
         @endif
     </div>
 </div>
@@ -22,9 +22,45 @@
     </div>
 </div>
 
+<div>
+    <form>
+        <div class="form-row align-items-center">
+            <div class="col-auto">
+                <label class="sr-only" for="calendarRange"></label>
+                <div class="input-group mb-2">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <i class="far fa-calendar-alt"></i>
+                        </div>
+                    </div>
+
+                    <input type="text" class="form-control" id="calendarRange" placeholder="Zeitraum">
+                </div>
+            </div>
+
+            <div class="col-auto">
+                <label class="sr-only" for="inlineFormInputGroup">Username</label>
+                <div class="input-group mb-2">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <i class="fas fa-search"></i>
+                        </div>
+                    </div>
+
+                    <input type="text" class="form-control" id="inlineFormInputGroup" placeholder="Username">
+                </div>
+            </div>
+
+            <div class="col-auto">
+                <button type="submit" class="btn btn-primary mb-2">Suchen</button>
+            </div>
+        </div>
+    </form>
+</div>
+
 <div class="row">
     <div class="col-12">
-        <table class="table table-striped">
+        <table class="table table-striped table-bordered">
             <thead class="table-dark">
                 <tr>
                     <td>Kontoinhaber</td>
@@ -35,7 +71,8 @@
                     <td>Aktionen</td>
                 </tr>
             </thead>
-            @foreach ($entries as $entry)
+            <tbody>
+                @foreach ($entries as $entry)
                 <tr>
                     <td> {{ $entry->account->account_holder }} </td>
                     <td> {{ $entry->category->category_name }} </td>
@@ -48,14 +85,21 @@
                             @method('delete')
                             @csrf
                             <button onclick="return confirm('Möchten Sie diesen Eintrag wirklich löschen?')"
-                                    class="btn btn-danger btn-sm floated" type="submit">Löschen
-                            </button>
+                            class="btn btn-danger btn-sm floated" type="submit">Löschen</button>
                         </form>
                     </td>
                 </tr>
-            @endforeach
+                @endforeach
+            </tbody>
         </table>
     </div>
 </div>
+
+<script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+<script type="text/javascript" src="js/calendar.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css"/>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"/>
 
 @endsection
